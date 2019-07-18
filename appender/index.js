970c77969b1f32fef8fd82c36a56dcf537d1fdc1
Reno McKenzie <Reno@RenoMcKenzie.com>
2019-07-17 20:30:42 -0700

    added research to start appender

---
+++ appender/index.js
@@ -0,0 +1,9 @@
+// resize content and styles
+// convert content.jpg style.png -resize 500x out.jpg
+
+// append content and style together
+// montage out-0.jpg out-1.jpg -mode concatenate -tile 2x1 -background black bottom.jpg
+
+// append result and "content/bottom.jpg" together
+// montage result.jpg bottom.jpg -mode concatenate -tile 1x2 final.jpg
+
